880d1dbe290ae99eae5dffd20ddad3ed3d175ef1
Eugenia Perotti <meugenia.perotti@gmail.com>
2020-01-23 11:19:56 -0300

    Te amo

---
+++ src/main/java/com/mycompany/prestamos/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prestamos;
+
+/**
+ *
+ * @author perotti1304
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDeCobranzaApplication.class, args);
+    }
+
+}
